957603c25c3a6e4f10ca30be511c4f860a120119
samuelepapiccio <80513527+samuelepapiccio@users.noreply.github.com>
2021-03-24 11:17:21 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -7,3 +7,7 @@ p224r63_2011 <-brick("p224r63_2011_masked.grd")
 p224r63_2011
 
 plot(p224r63_2011)
+
+#color change
+cl <- colorRampPalette(c('black','grey','light grey'))(100) 
+plot(p224r63_2011, col=cl)
