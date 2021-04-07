007a4fac02d48bbc3a4785d147857e0d4396ee38
samuelepapiccio <80513527+samuelepapiccio@users.noreply.github.com>
2021-04-07 13:05:14 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -4,3 +4,27 @@
 
 library(raster)
 setwd("/Users/samuelepapiccio/lab/greenland")
+
+#importo i file lst con la funzione raster
+lst_2000<-raster("lst_2000.tif")
+lst_2005<-raster("lst_2005.tif")
+lst_2010<-raster("lst_2010.tif")
+lst_2015<-raster("lst_2015.tif")
+
+#par
+par(mfrow=c(2,2))
+plot(lst_2000)
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
+
+#list of files
+list.files()
+
+rlist<-list.files(pattern="lst")
+
+import<-lapply(rlist,raster)
+
+TGr<-stack(import)
+plot(TGr)
+plotRGB(TGr,1,2,3,stretch="Lin")
