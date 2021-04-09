f304f66c58d573d6e7c80c2fecaa7adc64861779
samuelepapiccio <80513527+samuelepapiccio@users.noreply.github.com>
2021-04-09 10:59:44 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -2,7 +2,11 @@
 #Greenland increase of temperature
 #Data and code from Emanuela Cosma
 
+#install.packages("raster")
+#install.packages("rasterVis")
+
 library(raster)
+library(rasterVis)
 setwd("/Users/samuelepapiccio/lab/greenland")
 
 #importo i file lst con la funzione raster
@@ -20,11 +24,35 @@ plot(lst_2015)
 
 #list of files
 list.files()
-
 rlist<-list.files(pattern="lst")
 
+#apply a function to a list of files
 import<-lapply(rlist,raster)
 
 TGr<-stack(import)
 plot(TGr)
 plotRGB(TGr,1,2,3,stretch="Lin")
+
+levelplot(TGr)
+levelplot(TGr$lst_2000)
+cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
+levelplot(TGr, col.regions=cl)
+#rinomino i titoli delle singole immagini
+levelplot(TGr,col.regions=cl, names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+#inserisco il titolo
+levelplot(TGr,col.regions=cl, main="LST variation in time",names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+
+#melt
+#creo una lista
+meltlist<-list.files(pattern="melt")
+#applico la funzione raster alla lista
+import_melt<-lapply(meltlist,raster)
+melt<-stack(import_melt)
+
+levelplot(melt)
+
+melt_amount <- melt$X2007annual_melt - melt$X1979annual_melt
+levelplot(melt_amount)
+clb <- colorRampPalette(c("blue","white","red"))(100)
+plot(melt_amount, col=clb)
+levelplot(melt_amount, col.regions=clb)
