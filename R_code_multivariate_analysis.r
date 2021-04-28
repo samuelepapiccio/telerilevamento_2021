180f2bca7f4cc8d806a9faf7c9f479ecfa8dd351
samuelepapiccio <80513527+samuelepapiccio@users.noreply.github.com>
2021-04-28 12:44:39 +0200

    Update R_code_multivariate_analysis.r

--- R_code_multivariate_analysis.r
+++ R_code_multivariate_analysis.r
@@ -14,3 +14,17 @@ plot(p224r63_2011$B1_sre,p224r63_2011$B2_sre, col="red",pch=19,cex=2)
 
 #plottiamo tutte le correlazioni possibili con la funzione pairs
 pairs(p224r63_2011)
+
+
+#aggregate cells: resampling (ricampionamento)
+p224r63_2011res<-aggregate(p224r63_2011,fact=10,fun=mean)
+par(mfrow=c(2,1))
+plotRGB(p224r63_2011,r=4,g=3,b=2,stretch="lin")
+plotRGB(p224r63_2011res,r=4,g=3,b=2,stretch="lin")
+
+#RasterPCA: principal component analysis for rasters
+p224r63_2011res_PCA<-rasterPCA(p224r63_2011res)
+
+summary(p224r63_2011res_PCA$model)
+plot(p224r63_2011res_PCA$map)
+plotRGB(p224r63_2011res_pca$map, r=1, g=2, b=3, stretch="lin")
