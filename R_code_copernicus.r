15e8cee9c27ab18c24859626957e011372985f63
samuelepapiccio <80513527+samuelepapiccio@users.noreply.github.com>
2023-09-04 13:04:05 +0200

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -12,6 +12,6 @@ burnedarea<-raster("c_gls_BA300_202009200000_GLOBE_PROBAV_V1.1.1.nc")
 cl<-colorRampPalette(c("black","red","pink"))(100)
 plot(burnedarea,col=cl)
 
-#aggregate files, ricampionamento bi-lineare
+#aggregate files, resamplig bi-linear
 ba_res<-aggregate(burnedarea,fact=25)
 plot(ba_res,col=cl)
