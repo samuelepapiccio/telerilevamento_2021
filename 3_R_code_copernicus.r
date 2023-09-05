1ab8a599ddc74c66cc199b21b51636fe75cdda0e
samuelepapiccio <80513527+samuelepapiccio@users.noreply.github.com>
2023-09-05 09:19:03 +0200

    Update 3_R_code_copernicus.r

--- 3_R_code_copernicus.r
+++ 3_R_code_copernicus.r
@@ -15,3 +15,23 @@ plot(burnedarea,col=cl)
 #aggregate files, resamplig bi-linear
 ba_res<-aggregate(burnedarea,fact=25)
 plot(ba_res,col=cl)
+dev.off()
+
+#Albedo
+#install.packages("ncdf4")
+#library(raster)
+#library(ncdf4)
+setwd("/Users/samuelepapiccio/lab/")
+
+
+cl <- colorRampPalette(c('light blue','green','red','yellow'))(100) 
+cl <- colorRampPalette(c('light blue','green','red','yellow'))(100) 
+plot(albedo, col=cl)
+
+# resampling, fact=100 riduce di 100 pixels
+albedores <- aggregate(albedo, fact=100)
+plot(albedores, col=cl)
+
+
+
+
