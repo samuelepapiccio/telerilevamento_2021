771f7ce2d516e76fb00e79d62c010b566cbc48ec
samuelepapiccio <80513527+samuelepapiccio@users.noreply.github.com>
2023-09-05 09:29:12 +0200

    Update r_code_knitr.r

--- r_code_knitr.r
+++ r_code_knitr.r
@@ -3,5 +3,5 @@
 setwd("/Users/samuelepapiccio/lab/")
 library(knitr)
 
-#
+#crea un report  nome codice                                                    nome pacchetto     
 stitch("R_code_greenland.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
